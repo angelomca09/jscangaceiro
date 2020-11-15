64669e5bee2b40d6c453ddfe44650acc6cc63ea0
Angelo Munhoz <angelomca09@gmail.com>
2020-11-15 03:58:23 -0300

    Programação Defensiva em 'data'

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,12 +1,12 @@
 class Negociacao {
   constructor(data, quantidade, valor) {
-    this._data = data;
+    this._data = new Date(data.getTime()); // Programação Defensiva
     this._quantidade = quantidade;
     this._valor = valor;
   }
 
   get data() {
-    return this._data;
+    return new Date(this._data.getTime()); // Programação Defensiva
   }
 
   get quantidade() {
